bc8198f79e97ca4b1e4761ac3f998cf5976266c9
brunomotadev <brunomotadev@gmail.com>
2021-07-13 17:07:56 -0300

    confirm - javascript

--- javascript/console-alerts/script.js
+++ javascript/console-alerts/script.js
@@ -7,13 +7,24 @@
 // console.error(3);
 // console.info(4);
 
+// ALERT <--
 // let valor = "Bruno";
 // alert(1);
 // alert("Mensagem");
 // alert(valor);
 
+
+// PROMPT <--
 // let valor = prompt("Qual é o seu nome");
 // alert(valor);
-
 // alert("Olá, " + prompt("Qual é o seu nome?"));
+
+let idade = confirm("Você tem mais de 18 anos?");
+console.log(idade);
+
+let numero = prompt("Escolha um número:");
+
+while(numero * numero != prompt("Qual o resultado desse número multiplicado por ele mesmo?")){
+
+}
 
